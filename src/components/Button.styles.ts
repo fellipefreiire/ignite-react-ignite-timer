1001fa9fc85c6314fc6bb467974c66f75e0b0a8f
Felipe <fellipefreiire3@gmail.com>
2022-07-08 19:04:13 -0300

    theme

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,6 +16,10 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<IButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
   
-  background-color: ${({ variant }) => buttonVariants[variant]};
+  /* background-color: ${({ variant }) => buttonVariants[variant]}; */
+  background-color: ${({ variant, theme }) => theme[variant]};
 `

ea7166807151b0fd2f6e428fd601842b7c92feec
Felipe <fellipefreiire3@gmail.com>
2022-07-08 23:33:16 -0300

    input enhancing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,25 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <S.TaskInput
             id="task"
+            list="task-suggestions"
             type="text"
             placeholder="Dê um nome para o seu projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <S.MinutesAmountInput
             id="minutesAmount"
             type="number"
+            min={5}
+            max={60}
+            step={5}
             placeholder="00"
           />
 

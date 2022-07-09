0cfe41c948e6af0a48060cbb9742778f450c6a8f
Felipe <fellipefreiire3@gmail.com>
2022-07-08 23:42:36 -0300

    page history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,48 @@
+import * as S from './styles'
+
+export function History() {
+  return (
+    <S.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.HistoryContainer>
+  )
+}

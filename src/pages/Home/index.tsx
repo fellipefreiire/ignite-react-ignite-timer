f07dd1dd2c816edd3db5d70e6b8949eeaa32c97d
Felipe <fellipefreiire3@gmail.com>
2022-07-08 23:05:44 -0300

    home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from 'phosphor-react'
+import * as S from './styles'
+
+export function Home() {
+  return (
+    <S.HomeContainer>
+      <form action="">
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </S.HomeContainer>
+  )
+}

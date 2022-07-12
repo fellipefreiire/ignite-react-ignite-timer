2b17326be4b2ca866f75ce591b110de8454b8fcd
Felipe <fellipefreiire3@gmail.com>
2022-07-12 01:15:07 -0300

    Melhorar estrutura de imports e adicionar contexto de ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
+
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { CyclesContextProvider } from './store/context/CyclesContext'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -9,7 +11,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
